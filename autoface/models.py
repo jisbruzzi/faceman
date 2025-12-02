510b3d9be877a4bf404405df5e103e202795ffcf
jisbruzzi-cp <jsbruzzi@counselpress.com>
2025-12-02 16:41:28 -0300

    implementar primeras 3 entidades

--- autoface/models.py
+++ autoface/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Catedra(models.Model):
+    codigo = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+
+
+class Docente(models.Model):
+    legajo = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    dni = models.CharField(max_length=200)
+
+
+class Cargo(models.Model):
+    codigo = models.CharField(max_length=200)
+    estado = models.CharField(max_length=200)
+    puntos = models.IntegerField(default=0)
+    docente = models.ForeignKey(Docente, on_delete=models.PROTECT)
+    catedra = models.ForeignKey(Catedra, on_delete=models.PROTECT)
